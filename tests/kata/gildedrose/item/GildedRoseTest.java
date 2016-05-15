e41a8024991fa2627a9851e7cd88a8170d7b0f0c
rojoangel <rojo.angel@gmail.com>
2016-05-15 23:35:55 +0200

    refactor to use build item explaining method in gilded rose test

--- tests/kata/gildedrose/item/GildedRoseTest.java
+++ tests/kata/gildedrose/item/GildedRoseTest.java
@@ -17,27 +17,27 @@ public class GildedRoseTest {
     public static Object[][] provideItems() {
         return new Object[][] {
                 {"standard item - sellIn and quality lower at the end of each day",
-                        new StandardItem(10, 6), new StandardItem(9, 5)},
+                        new StandardItem(10, 6), buildItem("An standard item", 9, 5)},
                 {"standard item - once the sell date has passed quality degrades twice as fast",
-                        new StandardItem(0, 6), new StandardItem(-1, 4)},
+                        new StandardItem(0, 6), buildItem("An standard item", -1, 4)},
                 {"standard item - quality is never negative",
-                        new StandardItem(10, 0), new StandardItem(9, 0)},
+                        new StandardItem(10, 0), buildItem("An standard item", 9, 0)},
                 {"aged brie - quality increases the older it gets",
-                        new AgedBrie(10, 7), new AgedBrie(9, 8)},
+                        new AgedBrie(10, 7), buildItem("Aged Brie", 9, 8)},
                 {"aged brie - quality is never greater than 50",
-                        new AgedBrie(10, 50), new AgedBrie(9, 50)},
+                        new AgedBrie(10, 50), buildItem("Aged Brie", 9, 50)},
                 {"sulfuras - never has to be sold",
-                        new Sulfuras(99), new Sulfuras(99)},
+                        new Sulfuras(99), buildItem("Sulfuras", 99, 80)},
                 {"sulfuras - quality is 80 and never alters",
                         new Sulfuras(99), buildItem("Sulfuras", 99, 80)},
                 {"backstage pass - quality increases",
-                        new BackstagePass(15, 27), new BackstagePass(14, 28)},
+                        new BackstagePass(15, 27), buildItem("Backstage Pass", 14, 28)},
                 {"backstage pass - quality increases by 2 when sellIn is 10 days or less",
-                        new BackstagePass(10, 27), new BackstagePass(9, 29)},
+                        new BackstagePass(10, 27), buildItem("Backstage Pass", 9, 29)},
                 {"backstage pass - quality increases by 3 when sellIn is 5 days or less",
-                        new BackstagePass(6, 27), new BackstagePass(5, 30)},
+                        new BackstagePass(6, 27), buildItem("Backstage Pass", 5, 30)},
                 {"backstage pass - quality drops to zero after theConcert",
-                        new BackstagePass(0, 27), new BackstagePass(-1, 0)}
+                        new BackstagePass(0, 27), buildItem("Backstage Pass", -1, 0)}
         };
     }
 
