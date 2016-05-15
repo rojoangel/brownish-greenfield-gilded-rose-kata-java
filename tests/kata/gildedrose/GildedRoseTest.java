2d117d5afc020d8bb4317c3c2ca6c93171c51df0
rojoangel <rojo.angel@gmail.com>
2016-05-15 23:51:51 +0200

    refactor gilded rose test by moving it to kata.gildedrose package

--- tests/kata/gildedrose/GildedRoseTest.java
+++ tests/kata/gildedrose/GildedRoseTest.java
@@ -1,9 +1,12 @@
-package kata.gildedrose.item;
+package kata.gildedrose;
 
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
-import kata.gildedrose.Item;
+import kata.gildedrose.item.AgedBrie;
+import kata.gildedrose.item.BackstagePass;
+import kata.gildedrose.item.StandardItem;
+import kata.gildedrose.item.Sulfuras;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
