e43c4fc83a199272910c9ef4c96a0df2095fdf48
rojoangel <rojo.angel@gmail.com>
2016-05-16 12:51:47 +0200

    reformat gilded rose test to improve readability

--- tests/kata/gildedrose/GildedRoseTest.java
+++ tests/kata/gildedrose/GildedRoseTest.java
@@ -20,42 +20,84 @@ public class GildedRoseTest {
     public static Object[][] provideItems() {
         return new Object[][] {
                 {"standard item - sellIn and quality lower at the end of each day",
-                        new StandardItem(10, 6), buildItem("An standard item", 9, 5)},
+                        new StandardItem(10, 6),
+                        buildItem("An standard item", 9, 5)},
+
                 {"standard item - once the sell date has passed quality degrades twice as fast",
-                        new StandardItem(0, 6), buildItem("An standard item", -1, 4)},
+                        new StandardItem(0, 6),
+                        buildItem("An standard item", -1, 4)},
+
                 {"standard item - quality is never negative",
-                        new StandardItem(10, 0), buildItem("An standard item", 9, 0)},
+                        new StandardItem(10, 0),
+                        buildItem("An standard item", 9, 0)},
+
                 {"aged brie - quality increases the older it gets",
-                        new AgedBrie(10, 7), buildItem("Aged Brie", 9, 8)},
+                        new AgedBrie(10, 7),
+                        buildItem("Aged Brie", 9, 8)},
+
                 {"aged brie - quality is never greater than 50",
-                        new AgedBrie(10, 50), buildItem("Aged Brie", 9, 50)},
+                        new AgedBrie(10, 50),
+                        buildItem("Aged Brie", 9, 50)},
+
                 {"sulfuras - quality is 80 and never has to be sold",
-                        new Sulfuras(99), buildItem("Sulfuras", 99, 80)},
+                        new Sulfuras(99),
+                        buildItem("Sulfuras", 99, 80)},
+
                 {"backstage pass - quality increases",
-                        new BackstagePass(15, 27), buildItem("Backstage Pass", 14, 28)},
+                        new BackstagePass(15, 27),
+                        buildItem("Backstage Pass", 14, 28)},
+
                 {"backstage pass - quality increases by 2 when sellIn is 10 days or less",
-                        new BackstagePass(10, 27), buildItem("Backstage Pass", 9, 29)},
+                        new BackstagePass(10, 27),
+                        buildItem("Backstage Pass", 9, 29)},
+
                 {"backstage pass - quality increases by 3 when sellIn is 5 days or less",
-                        new BackstagePass(6, 27), buildItem("Backstage Pass", 5, 30)},
+                        new BackstagePass(6, 27),
+                        buildItem("Backstage Pass", 5, 30)},
+
                 {"backstage pass - quality drops to zero after theConcert",
-                        new BackstagePass(0, 27), buildItem("Backstage Pass", -1, 0)},
+                        new BackstagePass(0, 27),
+                        buildItem("Backstage Pass", -1, 0)},
+
                 {"conjured standard item - quality degrades twice as fast",
-                        new Conjured(new StandardItem(10, 6)), new Conjured(buildItem("An standard item", 9, 4))},
+                        new Conjured(new StandardItem(10, 6)),
+                        new Conjured(buildItem("An standard item", 9, 4))},
+
                 {"conjured standard item - once the sell date has passed quality degrades twice as fast",
-                        new Conjured(new StandardItem(0, 6)), new Conjured(buildItem("An standard item", -1, 2))},
+                        new Conjured(new StandardItem(0, 6)),
+                        new Conjured(buildItem("An standard item", -1, 2))},
+
                 {"conjured aged brie - quality increases the older it gets twice as fast",
-                        new Conjured(new AgedBrie(10, 7)), new Conjured(buildItem("Aged Brie", 9, 9))},
+                        new Conjured(new AgedBrie(10, 7)),
+                        new Conjured(buildItem("Aged Brie", 9, 9))},
+
                 {"conjured sulfuras - quality is 80 and never has to be sold",
-                        new Conjured(new Sulfuras(99)), new Conjured(buildItem("Sulfuras", 99, 80))},
+                        new Conjured(new Sulfuras(99)),
+                        new Conjured(buildItem("Sulfuras", 99, 80))},
+
                 {"conjured backstage pass - quality increases twice as fast",
-                        new Conjured(new BackstagePass(15, 27)), new Conjured(buildItem("Backstage Pass", 14, 29))},
+                        new Conjured(new BackstagePass(15, 27)),
+                        new Conjured(buildItem("Backstage Pass", 14, 29))},
+
                 {"conjured backstage pass - quality increases when sellIn is 10 days or less twice as fast",
-                        new Conjured(new BackstagePass(10, 27)), new Conjured(buildItem("Backstage Pass", 9, 31))},
+                        new Conjured(new BackstagePass(10, 27)),
+                        new Conjured(buildItem("Backstage Pass", 9, 31))},
+
                 {"conjured backstage pass - quality increases when sellIn is 5 days or less twice as fast",
-                        new Conjured(new BackstagePass(6, 27)), new Conjured(buildItem("Backstage Pass", 5, 33))},
+                        new Conjured(new BackstagePass(6, 27)),
+                        new Conjured(buildItem("Backstage Pass", 5, 33))},
+
                 {"conjured backstage pass - quality drops to zero after theConcert",
-                        new Conjured(new BackstagePass(0, 27)), new Conjured(buildItem("Backstage Pass", -1, 0))}
+                        new Conjured(new BackstagePass(0, 27)),
+                        new Conjured(buildItem("Backstage Pass", -1, 0))}
         };
+    }
+
+    @Test
+    @UseDataProvider( "provideItems" )
+    public void testEndOfDay(String scenario, Item item, Item expected) throws Exception {
+        item.endOfDay();
+        assertThat(scenario, item, equalTo(expected));
     }
 
     private static Item buildItem(final String name, final int sellIn, final int quality) {
@@ -65,13 +107,6 @@ public class GildedRoseTest {
 
             @Override
             public void endOfDaySellIn() {}
-};
-    }
-
-    @Test
-    @UseDataProvider( "provideItems" )
-    public void testEndOfDay(String scenario, Item item, Item expected) throws Exception {
-        item.endOfDay();
-        assertThat(scenario, item, equalTo(expected));
+        };
     }
 }
