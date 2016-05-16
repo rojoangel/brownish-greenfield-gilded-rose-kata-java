653e5a9dc84fef11b840e5689212f1e3bdbd4f04
rojoangel <rojo.angel@gmail.com>
2016-05-16 22:08:36 +0200

    segregate factory related methods to a factory class

--- src/kata/gildedrose/item/BaseItem.java
+++ src/kata/gildedrose/item/BaseItem.java
@@ -75,26 +75,6 @@ public abstract class BaseItem implements Item {
     }
 
     public static Item buildItem(String name, int sellIn, int quality) {
-        if (name.startsWith(CONJURED)) {
-            String itemName = name.substring(CONJURED.length() + 1);
-            return new Conjured(constructItem(itemName, sellIn, quality));
-        } else {
-            return constructItem(name, sellIn, quality);
-        }
-    }
-
-    private static Item constructItem(String name, int sellIn, int quality) {
-        switch (name) {
-            case STANDARD_ITEM:
-                return new StandardItem(sellIn, quality);
-            case AGED_BRIE:
-                return new AgedBrie(sellIn, quality);
-            case SULFURAS:
-                return new Sulfuras(sellIn);
-            case BACKSTAGE_PASS:
-                return new BackstagePass(sellIn, quality);
-            default:
-                throw new RuntimeException("Unknown item name : " + name);
-        }
+        return Factory.buildItem(name, sellIn, quality);
     }
 }
---
+++ src/kata/gildedrose/item/Factory.java
@@ -0,0 +1,36 @@
+package kata.gildedrose.item;
+
+import kata.gildedrose.Item;
+
+import static kata.gildedrose.item.AgedBrie.AGED_BRIE;
+import static kata.gildedrose.item.BackstagePass.BACKSTAGE_PASS;
+import static kata.gildedrose.item.Conjured.CONJURED;
+import static kata.gildedrose.item.StandardItem.STANDARD_ITEM;
+import static kata.gildedrose.item.Sulfuras.SULFURAS;
+
+public class Factory {
+
+    public static Item buildItem(String name, int sellIn, int quality) {
+        if (name.startsWith(CONJURED)) {
+            String itemName = name.substring(CONJURED.length() + 1);
+            return new Conjured(constructItem(itemName, sellIn, quality));
+        } else {
+            return constructItem(name, sellIn, quality);
+        }
+    }
+
+    private static Item constructItem(String name, int sellIn, int quality) {
+        switch (name) {
+            case STANDARD_ITEM:
+                return new StandardItem(sellIn, quality);
+            case AGED_BRIE:
+                return new AgedBrie(sellIn, quality);
+            case SULFURAS:
+                return new Sulfuras(sellIn);
+            case BACKSTAGE_PASS:
+                return new BackstagePass(sellIn, quality);
+            default:
+                throw new RuntimeException("Unknown item name : " + name);
+        }
+    }
+}
