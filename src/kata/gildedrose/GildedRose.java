e851325b1b27fedf8a70fdf6fbdeaa0602f04531
rojoangel <rojo.angel@gmail.com>
2016-05-16 23:45:00 +0200

    change gilded rose add method signature so clients do not need to know about item instantiation mechanisms

--- src/kata/gildedrose/GildedRose.java
+++ src/kata/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package kata.gildedrose;
 
+import kata.gildedrose.item.ItemFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,14 +9,16 @@ public class GildedRose {
 
     private final List<Item> items = new ArrayList<>();
 
-    public void add(Item item) {
-        items.add(item);
-    }
-
     public void endOfDay() {
         for (Item item : items) {
             item.endOfDaySellIn();
             item.endOfDayQuality();
         }
+    }
+
+    public Item add(String name, int sellIn, int quality) {
+        Item item = ItemFactory.build(name, sellIn, quality);
+        items.add(item);
+        return item;
     }
 }
--- tests/kata/gildedrose/GildedRoseTest.java
+++ tests/kata/gildedrose/GildedRoseTest.java
@@ -17,102 +17,107 @@ public class GildedRoseTest {
     public static Object[][] provideItems() {
         return new Object[][] {
                 {"standard item - sellIn and quality lower at the end of each day",
-                        ItemFactory.build("Standard Item", 10, 6),
+                        new Object[] {"Standard Item", 10, 6},
                         ItemFactory.build("Standard Item", 9, 5)},
 
                 {"standard item - once the sell date has passed quality degrades twice as fast",
-                        ItemFactory.build("Standard Item", 0, 6),
+                        new Object[] {"Standard Item", 0, 6},
                         ItemFactory.build("Standard Item", -1, 4)},
 
                 {"standard item - quality is never negative",
-                        ItemFactory.build("Standard Item", 10, 0),
+                        new Object[] {"Standard Item", 10, 0},
                         ItemFactory.build("Standard Item", 9, 0)},
 
                 {"aged brie - quality increases the older it gets",
-                        ItemFactory.build("Aged Brie", 10, 7),
+                        new Object[] {"Aged Brie", 10, 7},
                         ItemFactory.build("Aged Brie", 9, 8)},
 
                 {"aged brie - quality is never greater than 50",
-                        ItemFactory.build("Aged Brie", 10, 50),
+                        new Object[] {"Aged Brie", 10, 50},
                         ItemFactory.build("Aged Brie", 9, 50)},
 
                 {"sulfuras - quality is 80 and never has to be sold",
-                        ItemFactory.build("Sulfuras", 99, 80),
+                        new Object[] {"Sulfuras", 99, 80},
                         ItemFactory.build("Sulfuras", 99, 80)},
 
                 {"backstage pass - quality increases",
-                        ItemFactory.build("Backstage Pass", 15, 27),
+                        new Object[] {"Backstage Pass", 15, 27},
                         ItemFactory.build("Backstage Pass", 14, 28)},
 
                 {"backstage pass - quality increases by 2 when sellIn is 10 days or less",
-                        ItemFactory.build("Backstage Pass", 10, 27),
+                        new Object[] {"Backstage Pass", 10, 27},
                         ItemFactory.build("Backstage Pass", 9, 29)},
 
                 {"backstage pass - quality increases by 3 when sellIn is 5 days or less",
-                        ItemFactory.build("Backstage Pass", 6, 27),
+                        new Object[] {"Backstage Pass", 6, 27},
                         ItemFactory.build("Backstage Pass", 5, 30)},
 
                 {"backstage pass - quality drops to zero after theConcert",
-                        ItemFactory.build("Backstage Pass", 0, 27),
+                        new Object[] {"Backstage Pass", 0, 27},
                         ItemFactory.build("Backstage Pass", -1, 0)},
 
                 {"conjured standard item - quality degrades twice as fast",
-                        ItemFactory.build("Conjured Standard Item", 10, 6),
+                        new Object[] {"Conjured Standard Item", 10, 6},
                         ItemFactory.build("Conjured Standard Item", 9, 4)},
 
                 {"conjured standard item - once the sell date has passed quality degrades twice as fast",
-                        ItemFactory.build("Conjured Standard Item", 0, 6),
+                        new Object[] {"Conjured Standard Item", 0, 6},
                         ItemFactory.build("Conjured Standard Item", -1, 2)},
 
                 {"conjured aged brie - quality increases the older it gets twice as fast",
-                        ItemFactory.build("Conjured Aged Brie", 10, 7),
+                        new Object[] {"Conjured Aged Brie", 10, 7},
                         ItemFactory.build("Conjured Aged Brie", 9, 9)},
 
                 {"conjured sulfuras - quality is 80 and never has to be sold",
-                        ItemFactory.build("Conjured Sulfuras", 99, 80),
+                        new Object[] {"Conjured Sulfuras", 99, 80},
                         ItemFactory.build("Conjured Sulfuras", 99, 80)},
 
                 {"conjured backstage pass - quality increases twice as fast",
-                        ItemFactory.build("Conjured Backstage Pass", 15, 27),
+                        new Object[] {"Conjured Backstage Pass", 15, 27},
                         ItemFactory.build("Conjured Backstage Pass", 14, 29)},
 
                 {"conjured backstage pass - quality increases when sellIn is 10 days or less twice as fast",
-                        ItemFactory.build("Conjured Backstage Pass", 10, 27),
+                        new Object[] {"Conjured Backstage Pass", 10, 27},
                         ItemFactory.build("Conjured Backstage Pass", 9, 31)},
 
                 {"conjured backstage pass - quality increases when sellIn is 5 days or less twice as fast",
-                        ItemFactory.build("Conjured Backstage Pass", 6, 27),
+                        new Object[] {"Conjured Backstage Pass", 6, 27},
                         ItemFactory.build("Conjured Backstage Pass", 5, 33)},
 
                 {"conjured backstage pass - quality drops to zero after theConcert",
-                        ItemFactory.build("Conjured Backstage Pass", 0, 27),
+                        new Object[] {"Conjured Backstage Pass", 0, 27},
                         ItemFactory.build("Conjured Backstage Pass", -1, 0)}
         };
     }
 
     @Test
     @UseDataProvider( "provideItems" )
-    public void testEndOfDay(String scenario, Item item, Item expected) throws Exception {
+    public void testEndOfDay(
+            String scenario,
+            Object[] itemValues,
+            Item expected
+    ) throws Exception {
+
+        String name = (String) itemValues[0];
+        int sellIn = (int) itemValues[1];
+        int quality = (int) itemValues[2];
+
         GildedRose gildedRose = new GildedRose();
-        gildedRose.add(item);
+        Item item = gildedRose.add(name, sellIn, quality);
         gildedRose.endOfDay();
+
         assertThat(scenario, item, equalTo(expected));
     }
 
     @Test
     public void testEndOfDayWithMultipleItems() throws Exception {
         GildedRose gildedRose = new GildedRose();
-
-        Item standardItem = ItemFactory.build("Standard Item", 10, 6);
-        gildedRose.add(standardItem);
-
-        Item conjuredBackStagePass = ItemFactory.build("Conjured Backstage Pass", 0, 27);
-        gildedRose.add(conjuredBackStagePass);
+        Item standardItem = gildedRose.add("Standard Item", 10, 6);
+        Item conjuredBackStagePass = gildedRose.add("Conjured Backstage Pass", 0, 27);
 
         gildedRose.endOfDay();
 
         assertThat(standardItem, equalTo(ItemFactory.build("Standard Item", 9, 5)));
-        final int sellIn = -1;
-        assertThat(conjuredBackStagePass, equalTo(ItemFactory.build("Conjured Backstage Pass", sellIn, 0)));
+        assertThat(conjuredBackStagePass, equalTo(ItemFactory.build("Conjured Backstage Pass", -1, 0)));
     }
 }
